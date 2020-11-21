743c2b308138db6f9d7aecf803dae1c7871f7fa1
James Swineson <github@public.swineson.me>
2020-11-21 22:11:23 +0800

    fix adblocker detection

--- dist/TheOtherShoes.js
+++ dist/TheOtherShoes.js
@@ -1,6 +1,6 @@
 var TheOtherShoes = (function() {
     var isAdBlockerInstalled = function() {
-        if(document.getElementById('WOuzPZciTHsd')){
+        if(document.getElementById('553b9579-e5d8-47d4-b3ca-5cc49356a56e')){
             return false;
         } else {
             return true;
